19b15c6ab8cbfe08fe1c3445f1d9e65657d4878b
knowmadec <odic3o79@gmail.com>
2020-03-28 17:21:12 -0500

    first commit

---
+++ interVal1.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+# -*- coding: utf-8 -*-
+
+"""
+Algoritmo de intercambio.Este algoritmo intercambia los valores de a y b
+
+"""
+
+
+#INICIO
+#declaramos las variables a usar 
+A = 3.56
+B = -7.15
+
+C = A
+
+A = B
+
+B = C
+
+
+print("Los valores de A y B son :")
+print("El valor de A es: %5.3f " % A)
+print("El valor de B es: %5.3f " % B)
+
+#FIN
+  
+
+  
